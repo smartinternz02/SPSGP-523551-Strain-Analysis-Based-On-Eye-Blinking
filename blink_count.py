6fb61415cd0ca950f9527e11127e3900c9fe5cc9
Laasya Bulusu <91075748+laasya-dev@users.noreply.github.com>
2023-07-03 20:02:23 +0530

    Add files via upload

---
+++ blink_count.py
@@ -0,0 +1,169 @@
+import datetime
+
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+from gtts import gTTS
+import tkinter as tk
+from tkinter import ttk
+from playsound import playsound
+
+
+def playaudio(text, filename):
+    speech = gTTS(text)
+    print(type(speech))
+    filename = filename + ".mp3"
+    speech.save(filename)
+    playsound(filename)
+    return
+
+
+LARGE_FONT = ("Verdana", 12)
+NORM_FONT = ("Helvetica", 10)
+SMALL_FONT = ("Helvetica", 8)
+
+
+def popupmsg(msg):
+    popup = tk.Tk()
+    popup.title("Urgent Warning!!")
+    popup.geometry('250x100')
+
+    style = ttk.Style(popup)
+    style.theme_use('classic')
+    style.configure('Test.TLabel', background='aqua')
+
+    label = ttk.Label(popup, text=msg, font="Ariel 12 bold")
+    label.place(relx=0.5, rely=0.25, anchor="center")
+
+    B1 = ttk.Button(popup, text="Continue", command=popup.destroy)
+    B1.pack(side= "bottom")
+    popup.mainloop()
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    C = dist.euclidean(eye[0], eye[3])
+
+    ear = (A + B) / (2.0 * C)
+
+    return ear
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape_predictor", required=True,
+                help="path to facial landmark predictor")
+ap.add_argument("-v", "--video", type=str, default="",
+                help="path to input video file")
+args = vars(ap.parse_args())
+
+def eye_blink():
+    EYE_AR_THRESH = 0.24
+    EYE_AR_CONSEC_FRAMES = 4
+    eye_thresh = 11
+
+    COUNTER = 0
+    TOTAL = 0
+
+    print("[INFO] loading facial landmark predictor...")
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(args["shape_predictor"])
+
+
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    print("[INFO] starting video stream thread...")
+    vs = VideoStream(src=0).start()
+    fileStream = False
+    time.sleep(1.0)
+
+    before = datetime.datetime.now().minute
+
+    while True:
+        if fileStream and not vs.more():
+            break
+
+        frame = vs.read()
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        rects = detector(gray, 0)
+
+        for rect in rects:
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+
+            ear = (leftEAR + rightEAR) / 2.0
+
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+            if ear < EYE_AR_THRESH:
+                COUNTER += 1
+            else:
+
+                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                    TOTAL += 1
+
+                COUNTER = 0
+
+            now = datetime.datetime.now().minute
+            no_of_minute = now - before
+            print(no_of_minute, before, now)
+            blinks = no_of_minute * eye_thresh
+
+            if(TOTAL < blinks - eye_thresh):
+                playaudio("I see you have been staring at your screen for a while now.Try not to get your eyes strained!", "output1")
+                popupmsg("Take Rest for a while!!\nYou are not blinking enough.")
+                cv2.putText(frame, "Take Rest for a while",
+                            (70,150),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (0,0,255),
+                            2)
+                TOTAL= 0
+                before = datetime.datetime.now().minute
+
+            elif(TOTAL > blinks + eye_thresh):
+                playaudio("A little time off the screen might be good for you.Rest your eyes for a while.", "output2")
+                popupmsg("Take Rest for a while!!\n You are blinking too much.")
+                cv2.putText(frame, "Take Rest for a while",
+                            (70, 150),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,
+                            (0, 0, 255),
+                            2)
+                TOTAL = 0
+                before = datetime.datetime.now().minute
+
+
+            cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "EAR: {:.2f}".format(ear), (320, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "Press E to Exit".format(ear), (150, 330),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("e"):
+            break
+
+    cv2.destroyAllWindows()
+    vs.stop()
